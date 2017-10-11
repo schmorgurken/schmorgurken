969d868105bb9c159e6d78d478cdee314cff702f
schmorgurken <schmorgurken@anamatica.com>
2017-10-11 17:23:08 +0100

    Minor changes to support GPG signing for clojars deploy

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject schmorgurken "0.1.0"
-  :description "Native Gherkin BDD tool for Clojure"
+  :description "Native Clojure Gherkin BDD tool"
   :url "https://github.com/schmorgurken/schmorgurken"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [pjstadig/humane-test-output "0.8.2"]]
+  :jar-exclusions [#"test/.*" #"features/.*" #"empty/.*"]
+  :signing {:gpg-key "schmorgurken@anamatica.com"}
   :target-path "target/%s")
-
