2b5cb8d0495e07c3fd8f6e77ead8116adb674857
schmorgurken <schmorgurken@anamatica.com>
2017-10-11 10:56:46 +0100

    Fixed up some issues in project file

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject schmorgurken "0.1.0-SNAPSHOT"
+(defproject schmorgurken "0.1.0"
   :description "Native Gherkin BDD tool for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/schmorgurken/schmorgurken"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [pjstadig/humane-test-output "0.8.2"]]
-  :main ^:skip-aot schmorgurken.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s")
 
