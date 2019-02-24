31d900033471820c5431d1f18f26af2cd2138cbb
nick <nick@anamatica.com>
2019-02-24 09:13:32 +0000

    Bumped version of Clojure to 1.10

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject schmorgurken "0.1.1"
+(defproject schmorgurken "0.1.2"
   :description "Native Clojure Gherkin BDD tool"
   :url "https://github.com/schmorgurken/schmorgurken"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.8.3"]]}}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :jar-exclusions [#"test/.*" #"features/.*" #"empty/.*"]
   :signing {:gpg-key "schmorgurken@anamatica.com"}
   :target-path "target/%s")
